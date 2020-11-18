7e7a11f6797919b9298881f2ba13e947e99d7e1d
willelz <37827548+willelz@users.noreply.github.com>
2020-11-18 15:39:06 +0900

    add doc/jax

--- scripts/docgen.sh
+++ scripts/docgen.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
 ./to_vimdoc.sed ../README.md | fmt -s | ./sections_tags.awk > ../doc/nvim-lua-guide.txt
+./to_vimdocja.sed ../README.ja.md | fmt -s | ./sections_tags.awk > ../doc/nvim-lua-guide.jax
